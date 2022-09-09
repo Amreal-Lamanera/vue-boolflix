eaeb90cd9ad36bf42908eb5cd3c010f904bb6a5f
Francesco Pieraccini <pieraccini.francesco@gmail.com>
2022-09-09 13:14:05 +0200

    add content + query try

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li v-for="movie in movies" :key="movie.id">
+        <p>
+          {{ movie.title}}
+        </p>
+        <p>
+          {{movie.overview}}
+        </p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import axios from 'axios'
+
+  export default {
+    name: 'App',
+    components: {
+    },
+    data() {
+      return {
+        movies: [],
+        apiKey: '604eb69c5e2149adb681169e8dc9e532',
+        query: 'futuro',
+        baseUri:'https://api.themoviedb.org/3'
+      }
+    },
+    methods: {
+      fetchMovies() {
+        axios
+
+          // .get(`concatenazione_interpolazione_stringhe => ${this.baseUri}/search/movie?api_key=${this.apiKey}&query=${this.query}`)
+          // https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&page=1&include_adult=false
+
+          //alternativa
+          .get(`${this.baseUri}/search/movie`,{
+            params: {
+              api_key: this.apiKey,
+              query: this.query,
+              language: 'it-IT',
+            }
+          })
+
+          .then((res) => {
+            console.log(res.data);
+            this.movies = res.data.results;
+          })
+      }
+    },
+    beforeMount() {
+      this.fetchMovies();
+    },
   }
-}
+
 </script>
 
 <style lang="scss">
