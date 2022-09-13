a64c61074bf7f2995b2ffca40e00fe6f437b8a9a
Francesco Pieraccini <pieraccini.francesco@gmail.com>
2022-09-13 12:09:05 +0200

    bug fix

--- src/components/CardContainer.vue
+++ src/components/CardContainer.vue
@@ -57,6 +57,13 @@
     methods: {
         onResize() {
             this.windowWidth = window.innerWidth;
+            if(this.windowWidth < 768) this.dim = 2;
+            else if(this.windowWidth < 992) this.dim = 3;
+            else if(this.windowWidth < 1200) this.dim = 4;
+            else this.dim = 5;
+
+            if(this.windowWidth < 992) this.padding = 32;
+            else this.padding = 96;
         },
 
         moveNext() {
@@ -132,13 +139,13 @@
             // if(this.windowWidth < 576)
             //     this.dim = 1;
             // else
-            if(this.windowWidth < 768) this.dim = 2;
-            else if(this.windowWidth < 992) this.dim = 3;
-            else if(this.windowWidth < 1200) this.dim = 4;
-            else this.dim = 5;
+            // if(this.windowWidth < 768) this.dim = 2;
+            // else if(this.windowWidth < 992) this.dim = 3;
+            // else if(this.windowWidth < 1200) this.dim = 4;
+            // else this.dim = 5;
 
-            if(this.windowWidth < 992) this.padding = 32;
-            else this.padding = 96;
+            // if(this.windowWidth < 992) this.padding = 32;
+            // else this.padding = 96;
         })
     },
 }
