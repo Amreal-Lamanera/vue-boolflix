2529a9ee9bc78c99f2851de7c69855bcf8b0c539
francesco.pieraccini <francesco.pieraccini@vola.it>
2023-02-09 12:55:41 +0100

    commento per DEPLOY

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  // TODO: DA MODIFICARE A SECONDA DEL PATH DEL PROGETTO + QUANDO SI CARICA ONLINE ELIMINARE I VECCHI FILE PERCHE HANNO NOMI DIVERSI E RIMARREBBERO DOPPI CON RISCHIO DI ERRORE!!
+  publicPath: '/my_site/projects/boolflix/'
+}
